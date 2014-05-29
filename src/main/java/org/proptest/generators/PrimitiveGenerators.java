38e7d6a73480ee2ac713ffd9a601f9b73deb995d
Harald Krämer <tetha42@gmail.com>
2014-05-29 17:07:15 +0200

    Added Long Input Generator

--- src/main/java/org/proptest/generators/PrimitiveGenerators.java
+++ src/main/java/org/proptest/generators/PrimitiveGenerators.java
@@ -3,7 +3,8 @@ package org.proptest.generators;
 import java.util.Random;
 
 public class PrimitiveGenerators {
-
+ 
     public static final InputGenerator<Integer> INTEGERS = Random::nextInt;
-
+    public static final InputGenerator<Long> LONGS = Random::nextLong;
+    
 }
