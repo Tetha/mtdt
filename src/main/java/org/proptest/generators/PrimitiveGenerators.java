d1d9661f71996eb2180d9dc3a0aa77810f0f8aeb
Harald Krämer <tetha42@gmail.com>
2014-06-06 09:44:53 +0200

    Added all primitive generators

--- src/main/java/org/proptest/generators/PrimitiveGenerators.java
+++ src/main/java/org/proptest/generators/PrimitiveGenerators.java
@@ -2,9 +2,26 @@ package org.proptest.generators;
 
 import java.util.Random;
 
+/**
+ * The input generators in this class provide uniformly distributed values of their respective type.
+ * 
+ * @author hkraemer
+ *
+ */
 public class PrimitiveGenerators {
- 
+    public static final InputGenerator<Byte> BYTES = r -> {
+        return (byte) (r.nextInt( Byte.MAX_VALUE + (-Byte.MIN_VALUE) + 1) + Byte.MIN_VALUE);
+    };
+
+    public static final InputGenerator<Short> SHORTS = r -> {
+        return (short) (r.nextInt( Short.MAX_VALUE + (-Short.MIN_VALUE) + 1) + Short.MIN_VALUE);
+    };
+
     public static final InputGenerator<Integer> INTEGERS = Random::nextInt;
     public static final InputGenerator<Long> LONGS = Random::nextLong;
     
+    public static final InputGenerator<Float> FLOATS = Random::nextFloat;
+    public static final InputGenerator<Double> DOUBLES = Random::nextDouble;
+    
+    public static final InputGenerator<Boolean> BOOLEANS = Random::nextBoolean;
 }
