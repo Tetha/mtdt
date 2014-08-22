3831406fdd39cd1fc129b4a8abe92b26494e0202
Harald Kraemer <tetha42@gmail.com>
2014-08-22 20:59:18 +0200

    Added copyright informations

--- src/main/java/org/proptest/PropertyViolatedException.java
+++ src/main/java/org/proptest/PropertyViolatedException.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014 Harald Kraemer
+ *
+ * This file is part of "Multiple Threads Testing Disorder" or "MTDT".
+ *
+ * MTDT is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MTDT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with MTDT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.proptest;
 
 public class PropertyViolatedException extends RuntimeException {
