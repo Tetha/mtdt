13bc61186459a6e827aec4122a619a73e5b71091
Harald Kraemer <tetha42@gmail.com>
2014-08-24 15:27:38 +0200

    Added javadoc & Corrected package name

--- src/main/java/org/subquark/mtdt/generators/GeneratorsFromCollections.java
+++ src/main/java/org/subquark/mtdt/generators/GeneratorsFromCollections.java
@@ -17,12 +17,24 @@
  * along with MTDT.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package org.proptest.generators;
+package org.subquark.mtdt.generators;
 
 import java.util.List;
 
+/**
+ * This class contains generators based on the Java Collections Framework.
+ */
 public class GeneratorsFromCollections {
+    private GeneratorsFromCollections() {
+        /* static class. */
+    }
+
+    /**
+     * Randomly chooses one of the elements in the list according to a uniform distribution.
+     */
     public static <T> InputGenerator<T> oneOf( List<T> values ) {
+        if ( values == null ) throw new NullPointerException();
+
         return r -> {
             int idx = r.nextInt( values.size() );
             return values.get( idx );
